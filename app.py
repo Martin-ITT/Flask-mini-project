791a9c4928a3aa93e199b7583054c5ff5c54382e
Martin <x00135214@gmail.com>
2021-05-28 00:40:08 +0100

    conected to mongo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
-    redirect, request, url_for)
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 
 # mongo stores data in json like format bson
