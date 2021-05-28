6560c89217b7aad0f28a3d4683c9898f41132179
Martin <x00135214@gmail.com>
2021-05-28 20:47:43 +0100

    add login

--- app.py
+++ app.py
@@ -80,8 +80,38 @@ def register():
         # ours "user" for now.
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
-        
     return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username")})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    # This new 'user' cookie will be set to the form's username supplied, in all
+                    # lowercase to keep things simple, similar to how we store the username in our
+                    # database.
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+            
+                
+
+                                
+    return render_template("login.html")
 
 """
 The final step to test our application, is to tell our app how and where to run our application.
